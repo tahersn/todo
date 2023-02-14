4292bf36cb92531923c7d390a69dbcddff6ef95d
tahersn <taher.snoussi@esprit.tn>
2023-02-14 23:23:54 +0100

    update

--- src/components/EditingModal.jsx
+++ src/components/EditingModal.jsx
@@ -4,6 +4,7 @@ import useStore from "../store";
 function EditingModal({ todo, setEditing }) {
   const [text, setText] = React.useState(todo.text);
   const updateTodo = useStore((state) => state.updateTodo);
+
   return (
     <div className="flex flex-col items-center rounded-md bg-slate-800 p-3">
       <div>
@@ -17,7 +18,7 @@ function EditingModal({ todo, setEditing }) {
       <div className="items-center p-2 flex flex-row ">
         <div
           className="mr-1 px-3 py-2 bg-green-700 rounded cursor-pointer hover:bg-green-700/70 duration-200"
-          onClick={() => updateTodo(text)}
+          onClick={() => updateTodo(todo.id, text)}
         >
           Save
         </div>
